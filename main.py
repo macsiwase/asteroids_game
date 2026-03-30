eb23e2522c3c2e7b148cb93162136fac63e72d55
macsiwase <github.impairers@simplelogin.co>
2026-03-30 17:22:36 +0200

    added clock object and tick method

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -15,7 +17,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 if __name__ == "__main__":
     main()
